8f637be3adfbe429522be1d55c4d260f59fb1547
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2024-03-15 18:21:45 +0900

    設定周りを開発プロジェクトと揃えた

--- vue-with-markuplint/vite.config.js
+++ vue-with-markuplint/vite.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,6 +8,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: ['vue'],
+      dts: './src/auto-imports.d.ts',
+    }),
   ],
   resolve: {
     alias: {
